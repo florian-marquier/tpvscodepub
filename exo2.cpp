720632bda720755c9730366fff88cf1740f8676b
florian-marquier <florianmarquier1@gmail.com>
2023-09-21 11:08:47 +0200

    exo3

--- exo2.cpp
+++ exo2.cpp
@@ -1,1 +1,9 @@
-test test 
+#include <iostream>
+using namespace std; //Ceci est une ligne
+int main()
+/*Ceci est un commentaire
+sur plusieur lignes*/
+{
+    cout <<"Hello, World!"<< endl;
+    return 0;
+}
