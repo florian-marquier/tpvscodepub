1ed6a02fc5d4f1678d010d3c343f6bae2c54387c
florian-marquier <florianmarquier1@gmail.com>
2023-09-21 11:15:22 +0200

    exo4

--- exo4.cpp
+++ exo4.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int age_utilisateur(18);
-    cout <<"L'age de l'utilisateur est :"<<age_utilisateur<<"ans."<<endl;
-    
+    cout <<"L'age de l'utilisateur est :"<<age_utilisateur<<" ans."<<endl;
+
     return 0;
 }
