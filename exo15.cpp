b308ee87f69946c38e02c89ffa5e39e55bebf92f
florian-marquier <florianmarquier1@gmail.com>
2023-09-22 15:50:32 +0200

    exo15

---
+++ exo15.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool adulte(true);
+
+    if(adulte== true)
+    {
+        cout<<"Vous etes un adulte!"<<endl;
+    }
+    return 0;
+}
