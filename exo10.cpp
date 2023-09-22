8b7315678a4e842f9661a71d7dd73ee4acec64ed
florian-marquier <florianmarquier1@gmail.com>
2023-09-22 15:00:17 +0200

    exo10

---
+++ exo10.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string nom_prenom;
+    cout<<"Quel est votre nom et votre prenom ?"<<endl;
+    getline (cin,nom_prenom);
+    cout<<"Votre nom et votre prenom sont : "<< nom_prenom<<"."<<endl;
+    return 0;
+}
