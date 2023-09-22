df9ab5e0f15e3b15723233c4596b11d05c30b4ef
florian-marquier <florianmarquier1@gmail.com>
2023-09-22 15:31:14 +0200

    exo13

---
+++ exo13.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double counst nombres(16);
+    double resultat;
+    resultat=sqrt(nombres);
+    cout<<"La racine carre de "<< nombres <<" est "<<resultat<<endl;
+    return 0;
+}
