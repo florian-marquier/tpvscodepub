bda39dbac7b068b0960fb31c26ffec2ba751bd24
florian-marquier <florianmarquier1@gmail.com>
2023-09-22 16:25:49 +0200

    exo19

---
+++ exo19.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    vector<int> tableau(5,3);
+    tableau.push_back(8);
+    tableau.push_back(7);
+    tableau.push_back(6);
+    tableau.push_back();
+    tableau.push_back();
+    for (i=0; i<6; i++)
+    {
+        cout<<tableau[i]<<endl;
+    }
+    cout<<"Le tableau contient "<<tableau.size()<<" cases."<<endl;
+    return 0;
+}
